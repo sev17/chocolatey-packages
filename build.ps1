0bc7c3f14ed29aef6d783c2a434ea29a4f0f4971
sev17 <cmille19@gmail.com>
2016-04-22 15:40:38 -0400

    fix build.ps1
    fix build.ps1

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@
 # choco pack
 Get-ChildItem -Filter "*.DS_Store" -Force -Recurse | remove-item -force
 
-Get-ChildItem  -recurse | where {($_.psiscontainer) -and $_.name -ne 'tools' | select -expandproperty FullName | foreach { write-output "pushd $_; choco pack" }
+Get-ChildItem  -recurse | where {($_.psiscontainer) -and $_.name -ne 'tools'} | select -expandproperty FullName | foreach { write-output "pushd $_; choco pack" }
 
 #execute output above
