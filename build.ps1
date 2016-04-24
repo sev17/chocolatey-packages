dd3ed8264ebb5adefb36e1f974f70028821bafb1
Chad Miller <cmille19@gmail.com>
2016-04-24 14:12:21 -0400

    fix build.ps1
    fix build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 # Manual build
 # choco pack
+git clone https://github.com/sev17/chocolatey-packages.git
+
+get-childitem "C:\orainstall\chocolatey-packages" -Recurse | move-item -destination "C:\orainstall"
+
+rmdir c:\orainstall\chocolatey-packages -force
+
 Get-ChildItem -Filter "*.DS_Store" -Force -Recurse | remove-item -force
 
-Get-ChildItem  -recurse | where {($_.psiscontainer) -and $_.name -ne 'tools'} | select -expandproperty FullName | foreach { write-output "pushd $_; choco pack" }
+Get-ChildItem  -recurse | where {($_.psiscontainer) -and @('tools','icons','packages.config') -notcontains $_.name} | select -expandproperty FullName | foreach { write-output "pushd $_; choco pack" }
 
 #execute output above
